b06cc9fc058283faf8683fbfc0ab4cd583c93a7c
Nurali <nuyuldashov@gmail.com>
2022-04-30 10:23:06 +0500

    first

---
+++ Birthday/app.js
@@ -0,0 +1,238 @@
+
+
+//? Yana bir g'oya Day Calculator bu kun minut sekundlarni hissoblaydi
+
+
+const check = document.querySelector('.check'),
+consWeek = document.querySelector('#week'),
+consMonth = document.querySelector('#month'),
+age = document.querySelector('#age'),
+next = document.querySelector('.next_birthday'),
+yourAge = document.querySelector('.age'),
+famous = document.querySelector('.section'),
+inputD = document.querySelector('.date')
+
+
+
+// * Calculate birth day button click
+
+var calculateBirthday = ()=>{
+    
+
+    getCurrentDate()
+    getBirthDate()
+
+    
+    calcAge()
+    summary()
+
+    section()
+
+}
+
+// * Get birthday from User
+
+const getBirthDate = ()=>{
+    let inDate = inputD.value
+    if(inDate==null || inDate==''){
+        yourAge.textContent = "Choose a date please!";  
+        return false; 
+      }
+    date = new Date(inDate)
+    let birthDate = {}
+    //   console.log(birthDate)
+    birthDate.year = date.getYear()
+    birthDate.mon = date.getMonth() + 1
+    birthDate.day = date.getDate()
+    birthDate.full = date
+    return birthDate
+}
+
+// * Get current date from system
+
+function getCurrentDate() {
+    let d = new Date(),
+        cY = d.getYear(),
+        cD = d.getDate(),
+        cM = d.getMonth() + 1
+        const D = { year: cY, date: cD, month: cM }
+    return D
+}
+
+// * Calculate User Age
+
+const calcAge = ()=>{
+    let birthday = getBirthDate()
+    let current = getCurrentDate()
+    
+    //  Calculate Age
+    var ageUser = current.year - birthday.year
+    
+    // Calculate Month
+    if (current.month >= birthday.mon) {
+        var ageMonth = current.month - birthday.mon
+    }
+    else {
+        ageUser-- 
+        var ageMonth = 12 + current.month - birthday.mon
+    }
+    
+    //  Calculate Day
+    
+    if (current.date >= birthday.day) {
+        var ageDay = current.date - birthday.day
+    }
+    
+    else{
+        if (ageMonth == 0) {
+            ageMonth = 12;
+            ageUser--;
+        }
+        if(birthday.mon == 1 || birthday.mon == 3 || birthday.mon == 5 || birthday.mon == 7 || birthday.mon == 8 || birthday.mon == 10 || birthday.mon == 12) {
+            ageMonth--
+            var ageDay = 31 - birthday.day + current.date
+        }
+        if(birthday.mon == 2) {
+            ageMonth--
+            var ageDay = 28 - birthday.day + current.date     
+            ageDay = ageDay >= 28 ? ageDay - 28 && ageMonth++ : ageDay = ageDay
+
+        }
+        if(birthday.mon == 4 || birthday.mon == 6 || birthday.mon == 9 || birthday.mon == 11) {
+            ageMonth--
+            var ageDay = 30 - birthday.day + current.date
+            ageDay = ageDay >= 30 ? ageDay - 30 && ageMonth++ : ageDay = ageDay
+        }
+    }
+    yourAge.textContent = ''
+    yourAge.textContent = yourAge.textContent + ageUser + " Year " + ageMonth + ' Month ' + ageDay + ' Day'
+    
+    // * Calculate Next Birthday
+    if (current.month == birthday.mon && current.date == birthday.day) {     // Buni boshqacha yozish mumkin 
+        // next.textContent = 12 + ' Month ' + 0 + ' Day '
+        ageMonth = 12 
+        nextBirthday = 0
+        
+    }
+    else{
+        if(current.month == 1 || current.month == 3 || current.month == 5 || current.month == 7 || current.month == 8 || current.month == 10 || current.month == 12) {
+            ageMonth = 11 - ageMonth 
+            var nextBirthday = 31 - ageDay
+            
+            if (nextBirthday == 31) {
+                ageMonth++
+                nextBirthday = 0
+            }else{nextBirthday = nextBirthday}
+        }
+        if(current.month == 2) {
+            ageMonth = 11 - ageMonth 
+        var nextBirthday = 28 - ageDay
+        
+            if (nextBirthday == 28) {
+                ageMonth++
+                nextBirthday = 0
+            }else{nextBirthday = nextBirthday}
+        }
+        if(current.month == 4 || current.month == 6 ||current.month == 9 || current.month == 11) {
+            ageMonth = 11 - ageMonth 
+        var nextBirthday = 30 - ageDay
+        if (nextBirthday == 30) {
+            ageMonth++
+            nextBirthday = 0
+        }else{nextBirthday = nextBirthday}
+        }
+    }
+
+    next.textContent = ''
+    next.textContent = ageMonth + ' Month ' + nextBirthday + ' Day '
+}
+// * Calculate Summary Month, Week, Day, Hour, Minut
+
+const summary = ()=>{
+    let birthday = getBirthDate().full
+    let current = new Date()
+    
+    //calculate the difference of dates
+    let totalMillisecond = current.valueOf() - birthday.valueOf();
+    
+    let totalYear = Math.floor(totalMillisecond / 31557600000)
+    let totalMonths = Math.floor(totalMillisecond / 2629746000)
+    let totalWeeks = Math.floor(totalMillisecond / 604800000)
+    let totalDays = Math.floor(totalMillisecond / 86400000)
+    let totalHours = Math.floor(totalDays * 24)                 //! 100% chiqarish uchun totalmilisecundni 3600000 ga bo'lish kerak
+    let totalMinutes = Math.floor(totalHours * 60) 
+    
+    age.textContent = totalYear
+    consMonth.textContent = totalMonths
+    consWeek.textContent = totalWeeks
+    document.getElementById('day').textContent = totalDays
+    document.getElementById('hour').textContent = totalHours
+    document.getElementById('minute').textContent = totalMinutes
+    
+}
+
+// ?===================================================  Famous Section  =========================================================
+
+
+
+function section () {
+    let inp = new Date(inputD.value).getMonth()+1
+    famous.innerHTML = ''
+
+    fetch ('data.json') 
+    
+        .then (json => json.json())
+        .then (arra => generateFamous(arra, inp))
+}
+
+function generateFamous (data, mon) {
+    
+    const filteredData = data.filter(item => item.mon == mon) 
+    
+
+    
+    filteredData.forEach(e => {
+        console.log(e.name)
+        const card = `
+            <div class="card MyCard">
+                <img src="${e.img == null ? emptyImageURL : e.img}" class="card-img-top" alt="no found">
+                <div class="card-body">
+                <h5 class="card-title">${e.name}</h5>
+                <span class="span"> ${e.date} </span>
+                    <p class="card-text">${e.desc}</p>
+                </div>
+            </div>
+            `
+        famous.innerHTML += card
+    });
+    
+}
+
+function filterByMoth(value){
+    console.log(value)
+    let inp = value
+    famous.innerHTML = ''
+
+    fetch ('data.json') 
+    
+        .then (json => json.json())
+        .then (arra => generateFamous(arra, inp))
+}
+//?=============================  Day and Dark mode section  ===============================================
+
+
+
+// Get the root element
+// var r = document.styleSheets[1].cssRules[1].style[4]
+// console.log(r)
+
+
+// document.documentElement.style.setProperty("--primary-dark", "#5526ad");
+// document.documentElement.style.setProperty("--primary", "#342c79");
+// document.documentElement.style.setProperty("--primary-light", "#2b4567");
+// document.documentElement.style.setProperty("--greyLight-1", "#14171e");
+// document.documentElement.style.setProperty("--greyDark", "#3e4c6e");
+// document.documentElement.style.setProperty("--greyLight-2", "#222");
+// document.documentElement.style.setProperty("--white", "#444");
+
+
---
+++ RGB_Game/app.js
@@ -0,0 +1,83 @@
+
+function start(level = 3){
+    let cardsNumber = level 
+    const card = createCards(cardsNumber)
+    
+    let newBoxes = getCorrect(card)
+    
+    document.querySelector("#root").innerHTML = ''
+    newBoxes.forEach(e => {
+        let b = `<div class="box" onclick="getChoose(this)" style="background-color: rgb(${e.color});" data-correct="${e.isTrue}"></div>`
+        document.querySelector("#root").innerHTML += b
+        });
+}
+    
+    function getRGB() {
+        let red = Math.round(Math.random() * 255)
+        let green = Math.round(Math.random() * 255)
+        let blue = Math.round(Math.random() * 255)
+        let rgb = `${red},${green},${blue}`
+        return rgb
+    }
+    function Card(color, isTrue=false) {
+        this.color = color
+        this.isTrue = isTrue
+    }
+    function createCards(n) {
+        const cards = []
+        for(let i = 0; i < n; i++) {
+            cards.push(new Card(getRGB() ))
+        }
+        return cards
+    }
+
+    function getCorrect (arr) {
+        let random = Math.floor(Math.random() * arr.length)
+        arr[random].isTrue = true
+        let outHeader =    `
+        <div class="color">(${arr[random].color})</div>
+        `
+        document.querySelector('.rgb').innerHTML = `
+                                                        <div class="r">R</div>
+                                                        <div class="g">G</div>
+                                                        <div class="b">B</div>
+                                                    `
+        document.querySelector('.rgb').innerHTML += outHeader
+        return arr
+    }
+
+function getChoose(element) {
+    let life = eval(document.querySelector('.life').textContent)
+    let score = eval(document.querySelector('.score').textContent)
+    let e = element.getAttribute("data-correct")
+    let s
+    let l
+    if (e == 'false'){
+        element.style.visibility = 'hidden'
+        l = +life - 1
+        document.querySelector('.life').textContent = l
+        if (l == 0){
+            document.querySelector('.over')
+            .style.display = 'flex'
+        }
+    } else {
+        s = +score + 1
+        let level = s < 6 ? 3 : s < 9 ? 6 : 9
+        document.querySelector('.score').textContent = s
+        document.querySelector('#score').textContent = s
+        document.querySelectorAll(".box").forEach(e => {
+            e.style.backgroundColor = element.style.backgroundColor
+            e.style.visibility = 'visible'
+        })
+        setTimeout(()=>{start(level)}, 2000)
+    }
+
+}
+function restart() {
+    document.querySelector('.life').textContent = 8
+    document.querySelector('.score').textContent = 0
+    document.querySelector('.over').style.display = 'none'
+    start()
+}
+
+
